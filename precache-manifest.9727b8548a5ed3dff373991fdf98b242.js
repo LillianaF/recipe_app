1cb00f093317868d1a9968736ecd620044c14a99
Lilliana Frantz <lillianafrantz@Sophies-Air-2.maine.rr.com>
2020-09-08 15:29:41 -0400

    Updates

---
+++ precache-manifest.9727b8548a5ed3dff373991fdf98b242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1fee4449f8be15b3370c6d6a72a774",
+    "url": "/recipe_app/index.html"
+  },
+  {
+    "revision": "bda800304716000ee3be",
+    "url": "/recipe_app/static/css/main.17f78737.chunk.css"
+  },
+  {
+    "revision": "6a126c68e2d555c63a08",
+    "url": "/recipe_app/static/js/2.502e01e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe_app/static/js/2.502e01e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda800304716000ee3be",
+    "url": "/recipe_app/static/js/main.b8b1690e.chunk.js"
+  },
+  {
+    "revision": "7581057984edd57b9500",
+    "url": "/recipe_app/static/js/runtime-main.52e9e9b6.js"
+  }
+]);
